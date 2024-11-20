074c0760f176545e7071eb419face6375fdb5817
AleksandrSafarov <aleks.safarov2005@gmail.com>
2024-11-20 09:15:07 +0300

    add create card

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+const createCard = (cardContent) => {
+    const card = cardTemplate.querySelector(".places__item").cloneNode(true);
+    const cardTitle = card.querySelector(".card__title");
+    const cardImage = card.querySelector(".card__image");
+    const likeButton = card.querySelector(".card__like-button");
+    const deleteButton = card.querySelector(".card__delete-button");
 
-// @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+    cardTitle.textContent = cardContent.name;
+    cardImage.src = cardContent.link;
+    cardTitle.alt = cardContent.name;
+    return card;
+};
+
+const cardsContainer = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+initialCards.forEach(function (item) {cardsContainer.append(createCard(item));
+    console.log(1);
+});
+
